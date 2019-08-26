9ae09843c3d2dc8d73d4c03899efc36a6be556ae
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 14:37:40 -0400

    Create three Points in three different ways.

--- ObjectInitializers/Program.cs
+++ ObjectInitializers/Program.cs
@@ -10,6 +10,22 @@ namespace ObjectInitializers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            // Make a Point by setting each property manually.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // Or make a Point via a custom constructor.
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            // Or make a Point using object init syntax.
+            Point finalPoint = new Point A{ X = 30, Y = 30};
+            finalPoint.DisplayStats();
+            Console.ReadLine();
         }
     }
 }
